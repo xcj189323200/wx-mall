dd223a4361427d3dbb5709cab67f278b51950a47
xuchangjian <xuchangjian@hefantv.com>
2019-03-22 12:58:58 +0800

    初始化

---
+++ src/pages/home/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id='home'>
+    <div class="home-banner">
+      <swiper autoplay>
+        <swiper-item  v-for='item in imgUrls' :key="item">
+          <image  :src="item" class="slide-image" width="355" height="150" />
+        </swiper-item>
+      </swiper>
+    </div>
+    <div class="home-cont">
+      <div class="home-cont-titile">这是title</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      imgUrls: [
+        'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+      ]
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+
+  },
+
+  created () {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+#home{
+  .home-banner{
+    color: red;
+  }
+  .home-cont{
+    background: #000;
+  }
+}
+</style>
---
+++ src/pages/order/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>订单列表</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+
+  },
+
+  created () {
+    console.log('sdasd')
+  }
+}
+</script>
+
+<style scoped>
+</style>
