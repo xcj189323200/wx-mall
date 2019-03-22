2fd3c7df48a9fec8ff3b4349c0b56def5f89dea4
xuchangjian <xuchangjian@hefantv.com>
2019-03-22 22:04:16 +0800

    添加商品详情

---
+++ src/pages/goods/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id='goodsDetails'>
+    这是商品详情
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+
+  },
+
+  created () {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+#goodsDetails{
+
+}
+</style>
