34e89c718522f6c14119c29af7656bc15ee49070
xuchangjian <xuchangjian@hefantv.com>
2019-03-23 17:38:43 +0800

    合并master

--- src/pages/goods/index.vue
+++ src/pages/goods/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div id='goodsDetails'>
-    这是商品详情
+      <div class="goods_banner">
+        <img src="../../../static/images/goods/20190323162858.png" class="goods_banner-image" mode='widthFix'>
+      </div>
   </div>
 </template>
 
@@ -28,6 +30,13 @@ export default {
 
 <style lang='less' scoped>
 #goodsDetails{
-
+  width:1500rpx;
+  .goods_banner{
+    width:1500rpx;
+    .goods_banner-image{
+      width:750rpx;
+      height:750rpx;
+    }
+  }
 }
 </style>
